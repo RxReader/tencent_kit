9a9a2971cbe3a45fd49d97eea4d050d8ea72a5a6
v7lin <v7lin@qq.com>
2019-12-12 23:52:02 +0800

    升级CI镜像

--- test/tencent_kit_test.dart
+++ test/tencent_kit_test.dart
@@ -2,9 +2,9 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
   const MethodChannel channel = MethodChannel('v7lin.github.io/tencent_kit');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {});
