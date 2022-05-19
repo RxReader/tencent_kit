d7d3967ba813efd7a298e6f5c46bd9fb4a5ad353
v7lin <v7lin@qq.com>
2022-05-19 09:44:20 +0800

    Update tencent_kit_platform_interface.dart

--- lib/tencent_kit_platform_interface.dart
+++ lib/tencent_kit_platform_interface.dart
@@ -1,4 +1,6 @@
 library tencent_kit_platform_interface;
 
+export 'src/model/resp.dart';
+export 'src/tencent_constant.dart';
 export 'src/tencent_kit_method_channel.dart';
 export 'src/tencent_kit_platform_interface.dart';
